db22e5d666572355d422cecf4fbae3f96347287d
adrian <sadrians@gmail.com>
2016-12-14 14:35:47 +0100

    Refactor generated.html to include reference to splitLines.js. However, function getSplitString could not be moved.

--- authoringtool/sequencelistings/static/sequencelistings/output/resources/splitLines.js
+++ authoringtool/sequencelistings/static/sequencelistings/output/resources/splitLines.js
@@ -27,3 +27,32 @@ function writeSplitString(aStr) {
         document.write(currentString + cntr + "<br />");
     }
 }
+
+// function getSplitString(aStr) {
+//     var res = "";
+//     var li = splitString(aStr);
+//     var cntr = 0;
+//     var groupIndex = 0;
+//     var currentString = "";
+//     var i;
+//     var list_length = li.length;
+// 
+//     for (i = 0; i < list_length; i++)
+//     {
+//         cntr += li[i].length;
+//         groupIndex += 1;
+//         currentString += li[i] + " ";
+//         if (groupIndex == 6)
+//         {
+//             res = res + currentString + cntr + "<br />";
+//             currentString = "";
+//             groupIndex = 0;
+//         }
+// 
+//     }
+//     if (currentString !== "") {
+//         res = res + currentString + cntr + "<br />";
+//     }
+// 
+//     return res;
+// }
