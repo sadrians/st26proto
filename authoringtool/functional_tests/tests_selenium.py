e5c93095d8e44609a885a6122dafe43bde38061b
adrian <sadrians@gmail.com>
2018-08-05 14:25:02 +0200

    Add selenium test for access to home page.

--- authoringtool/functional_tests/tests_selenium.py
+++ authoringtool/functional_tests/tests_selenium.py
@@ -70,23 +70,29 @@ class VisitorTest(LiveServerTestCase):
              
     def tearDown(self):
         self.browser.quit()
-                     
+
+    def test_can_access_home_page(self):
+        print 'Selenium: Running %s ...' % self._testMethodName
+
+        self.browser.get(
+            '%s%s' % (self.live_server_url, '/sequencelistings'))
+
+        self.assertIn('st26proto - Index', self.browser.title)
+        self.assertIn('WELCOME',
+                      self.browser.find_element_by_id('welcomeHeader').text)
+        self.assertIn('Disclaimer',
+                      self.browser.find_element_by_id('disclaimerHeader').text)
+
     def test_can_access_overview_page_no_seqls(self):
         print 'Selenium: Running %s ...' % self._testMethodName
                 
         self.browser.get('%s%s' %(self.live_server_url, '/sequencelistings/overview'))
                 
         self.assertIn('st26proto - Overview', self.browser.title) 
-                
-#         headers_h2 = self.browser.find_elements_by_tag_name('h2')
-#         self.assertIn('WELCOME', [h.text for h in headers_h2])
-#         self.assertIn('SEQUENCE LISTING PORTOFOLIO', [h.text for h in headers_h2])
-                      
-        no_seqls_par = self.browser.find_element_by_id('no_seqls_par')  
-        self.assertEqual('No sequence listings are available.', no_seqls_par.text)
-        self.assertEqual(0, len(self.browser.find_elements_by_tag_name('table')), 
+        self.assertIn('No sequence listings are available.', self.browser.find_element_by_id('no_seqls_par').text)
+        self.assertEqual(0, len(self.browser.find_elements_by_tag_name('table')),
                          'There should be no table if no seqls created.')
-            
+        # TODO: 20180805 currently there is no registration any longer ...
 #         unregistered visitors are not allowed to add seqls i.e. there is no link to add seql
         self.assertEqual(0, len(self.browser.find_elements_by_id('add_seql_link')))
 
